9070d636d3efe428b66bdb7b4027e085604dd647
ChoiYongWon <yongwon0824@naver.com>
2022-10-30 02:08:40 +0900

    test: healthcheck test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,9 +5,15 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get("healthcheck")
+  healthCheck(): string {
+    Logger.log("/healthcheck 헬스체크 로그입니다.");
+    return this.appService.getHealthCheck();
+  }
+
   @Get()
   getHello(): string {
-    Logger.log("/ 테스트 로그입니다.");
+    Logger.log("/ 메인 로그입니다.");
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return '<h1>우아한 인연들 Devops 구축 완료</h1>';
   }
+
+  getHealthCheck(): string {
+    return '<h1>우아한 인연들 서버는 건강합니다.</h1>';
+  }
 }
