206621aa701aeb4c4c351441ce0fd6c9aae2384a
ChoiYongWon <yongwon0824@naver.com>
2022-11-01 03:17:05 +0900

    test: CICD test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,10 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h1>우아한 인연들 Devops 구축 완료</h1>';
+    return '<h1>우연한 인연들 Devops 구축 완료</h1>';
   }
 
   getHealthCheck(): string {
-    return '<h1>우아한 인연들 서버는 건강합니다.</h1>';
+    return '<h1>우연한 인연들 서버는 건강합니다.</h1>';
   }
 }
