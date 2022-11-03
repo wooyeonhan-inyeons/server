610205dbe1f23e0351d0b16e62399b41797151e4
binbinjji <yb080707@naver.com>
2022-11-03 19:04:04 +0900

    feat: #6 DB 연결 완료

---
+++ src/admin/admin.entity.ts
@@ -0,0 +1,11 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Admin {
+  @PrimaryColumn({ length: 20 })
+  id: string;
+
+  @Column({ length: 500 })
+  pw: string;
+
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
