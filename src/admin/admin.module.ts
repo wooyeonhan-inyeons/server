268719eb41dc1a77cbc4a7b79437e333f78072b1
ChoiYongWon <yongwon0824@naver.com>
2022-11-04 17:49:33 +0900

    feat: #6 @nest/typeorm 로 변경

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Admin } from './admin.entity';
+import { AdminService } from './admin.service';
+import { AdminController } from './admin.controller';
 
-@Module({})
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Admin])],
+    providers: [AdminService],
+    controllers: [AdminController]
+})
 export class AdminModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminModule } from './admin/admin.module';
 import { ConfigModule } from '@nestjs/config';
+import { Admin } from './admin/admin.entity';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: process.env.NODE_ENV === 'prod' ? '.prod.env' : '.dev.env',
-  }), DatabaseModule, AdminModule],
+  }), TypeOrmModule.forRoot({
+    type: 'mariadb',
+    host: `${process.env.DB_URL}`,
+    port: Number(process.env.DB_PORT),
+    username: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_NAME}`,
+    entities: [Admin],
+    synchronize: true,
+  }), AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
