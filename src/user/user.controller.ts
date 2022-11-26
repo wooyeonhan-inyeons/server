fc3dc5c0285da93d43aff59dd87443b23f76a687
ChoiYongWon <yongwon0824@naver.com>
2022-11-26 19:22:08 +0900

    feat: #29 어드민 유저 어드민 폴더로 이동 및 userGetAll 명세서 수정

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -18,44 +18,9 @@ export class UserController {
 
     constructor(private userService: UserService){
 
+      
+
     }
 
-// 생성
-  @Post()
-  @Roles([Role.Admin])
-  async createUser(@Body() createUserData: RequestCreateUserDto) {
-    return await this.userService.create(createUserData);
-  }
 
-  // 조회
-  @Get('/all')
-  @ApiCreatedResponse({ status: 200, type: ResponseReadUserDto, isArray: true, description: "모든 유저를 조회합니다."})
-  @Roles([Role.Admin])
-  async getUserAll(): Promise<User[]> {
-    return await this.userService.findAll();
-  }
-
-  @Get()
-  @ApiCreatedResponse({ status: 200, type: ResponseReadUserDto, description: "user_id의 유저를 조회합니다." })
-  @Roles([Role.Admin])
-  async getUser(@Query('user_id') userID: string) {
-    return await this.userService.find(userID);
-  }
-
-  // 수정
-  @Patch()
-  @Roles([Role.Admin])
-  async patchUser(
-    @Query('user_id') userID: string,
-    @Body() updateData: RequestUpdateUserDto,
-  ) {
-    return await this.userService.update(userID, updateData);
-  }
-
-  // 삭제
-  @Delete()
-  @Roles([Role.Admin])
-  async removeUser(@Query('user_id') userID: string) {
-    return await this.userService.delete(userID);
-  }
 }
