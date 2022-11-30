66b888baf58afa128aa2c3f8d0b48e7fe26a54ad
yeonbin <yb080707@naver.com>
2022-11-30 16:39:14 +0900

    feat: #34 재미있는 포스팅 만들기

--- src/posting/posting.controller.ts
+++ src/posting/posting.controller.ts
@@ -1,6 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Post,Get, Query, UseGuards, Logger, Delete, Req,    } from '@nestjs/common';
+import { Posting } from './posting.entity';
+import { User } from 'src/user/user.entity';
 import { PostingService } from './posting.service';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Roles } from 'src/libs/decorators/roles.decorator';
+import { Role } from 'src/libs/enums/role.enum';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from 'src/auth/guards/local-auth.guard';
 
 @Controller('posting')
+@UseGuards(RolesGuard)
+@UseGuards(JwtAuthGuard)
 export class PostingController {
+
+    constructor(private postingService: PostingService){
+
+    }
+
+    @Roles([Role.User])
+    @Get('/:post_id') //post_id를 통해 posting에 있는 데이터를 끌어옴
+    async getPostingById(@Query('post_id') post_id: string) {
+        return await this.postingService.getPostingById(post_id);
+    }
+
+    
+    // 유저의 토큰 사용필요
+    @Roles([Role.User])
+    @Get() //user_id를 통해 그 유저가 작성한 posting들을 끌어옴
+    async getAllPostings(@Req() req){
+        return this.getAllPostings(req.user.user_id);
+    }
+    
+    
+    @Roles([Role.User])
+    @Delete() //post_id를 사용해 그 게시물 삭제함
+    async deletePosting(@Req() req){
+        this.postingService.deletePosting(req.User.post_id);
+        return;
+    }
+
 }
--- src/posting/posting.service.ts
+++ src/posting/posting.service.ts
@@ -1,13 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Friends } from 'src/friends/friends.entity';
+import { User } from 'src/user/user.entity';
+import { QueryBuilder, Repository } from 'typeorm';
 import { Posting } from './posting.entity';
+
+
 @Injectable()
 export class PostingService {
     constructor(
         @InjectRepository(Posting)
-        private usersRepository: Repository<Posting>,
+        private postingRepository: Repository<Posting>,
+
+        @InjectRepository(Friends)
+        private friendsRepository: Repository<Friends>
       ) {}
+
+      async getPostingById(post_id: string): Promise<Posting>{ //post_id를 통해 posting에 있는 데이터를 끌어옴
+        const found = await this.postingRepository.findOne({
+          where: {
+            post_id
+          }
+        })
+        if(!found) throw new NotFoundException();
+
+        return this.postingRepository.findOne({
+          where: {
+            post_id,
+          },
+        });
+      }
+
+      async getAllPostings(user_id: string): Promise<Posting[]>{ //user_id를 통해 그 유저가 작성한 posting들을 끌어옴
+        
+        const queryBuilder = this.postingRepository.createQueryBuilder('posting');
+        queryBuilder.where('posting.user_id=:user_id',{user_id: user_id});
+        const postings = await queryBuilder.getMany();
+
+        return postings;
+      }
+
+      async deletePosting(post_id: string): Promise<void>{ //post_id를 사용해 그 게시물 삭제함
+        const result = await this.postingRepository.delete({post_id});
+      }
+
+      async checkFriend(user_id: string, friend_id: string): Promise<Friends>{ //궁금한점: follower = friend_id && following = user_id도 만들어야하는지..?
+        const isFriend = await this.friendsRepository
+        .createQueryBuilder('friend')
+        .where('friend.follower =: user_id',{user_id: user_id})
+        .andWhere('friend.following =: friend_id', {friend_id: friend_id})
+        .andWhere('friend.relation_type=1')
+        .getOne();
+
+        return isFriend;
+      }
+  
+
 
 }
 
