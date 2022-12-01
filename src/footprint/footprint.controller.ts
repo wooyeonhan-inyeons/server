c2458394b0ec6628b406ca3c8b2114cdd0cb7757
lua <doezri@naver.com>
2022-12-01 20:44:36 +0900

    feat:#35 발자국 추가 및 조회

--- src/footprint/footprint.controller.ts
+++ src/footprint/footprint.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Req, Query } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { FootprintService } from './footprint.service';
 
 @Controller('footprint')
