e54f10482b5338afbffd9d693350c58b405a8283
lua <doezri@naver.com>
2022-12-01 20:42:17 +0900

    feat:#35 발자국 추가

--- src/footprint/footprint.controller.ts
+++ src/footprint/footprint.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
+import { FootprintService } from './footprint.service';
 
 @Controller('footprint')
-export class FootprintController {}
+export class FootprintController {
+    constructor(
+        private footprintService: FootprintService
+    ) { }
+
+    // 발자국 수 추가
+    @Post()
+    async updateFootPrint(@Query() query) {
+        return await this.footprintService.addFootprint(
+            query.user_id,
+            query.post_id,
+        );
+    }
+
+    // 발자국 수 조회
+    @Get()
+    async getFootprints(@Query() query) {
+        return await this.footprintService.getFootprints(query.post_id);
+    }
+}
