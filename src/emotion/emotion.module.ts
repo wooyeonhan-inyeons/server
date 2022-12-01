ff32cb473d69b240ff974a0dfa4d7498b40b1f7e
tph01415 <juse0204@gmail.com>
2022-12-01 17:51:07 +0900

    :memo: controller/service add

--- src/emotion/emotion.module.ts
+++ src/emotion/emotion.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Emotion } from './emotion.entity';
+import { EmotionController } from './emotion.controller';
+import { EmotionService } from './emotion.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Emotion])],
+  controllers: [EmotionController],
+  providers: [EmotionService],
 })
 export class EmotionModule {}
