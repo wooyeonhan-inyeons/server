2fffb5ff56c6f638962ffefa9855024a5505f98f
lua <doezri@naver.com>
2022-12-08 15:47:23 +0900

    feat: #45 알림 기능 틀

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,7 +1,53 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, InternalServerErrorException, Post, Query, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/libs/decorators/roles.decorator';
+import { Role } from 'src/libs/enums/role.enum';
+import { NotificationService } from './notification.service';
 
+@ApiTags('notification')
+@ApiBearerAuth()
+@UseGuards(RolesGuard)
+@UseGuards(JwtAuthGuard)
 @Controller('notification')
 export class NotificationController {
+  constructor(private notificationService: NotificationService) { }
   //user_id에 따라 알림을 불러오는 Controller
   //user_id, notification_id를 받아서 알림 확인을 체크하는 로직
+
+  // 알림 불러오기
+  @Get()
+  @ApiOperation({
+    summary: '알림을 불러옵니다.',
+  })
+  @Roles([Role.User])
+  async LoadNotification(@Req() req) {
+    return await this.notificationService
+      .loadNotifications(req.user.user_id).catch((err) => {
+        throw new InternalServerErrorException({
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: err.messages,
+        });
+      });
+  }
+
+  // 알림 확인 체크
+  @Post()
+  @ApiOperation({
+    summary: '알림을 확인했는지 체크합니다.',
+  })
+  @Roles([Role.User])
+  async CheckRead(@Query('notification_id') notification_id: string, @Req() req) {
+    return await this.notificationService
+      .isRead(notification_id, req.user.user_id)
+      .catch((err) => {
+        throw new InternalServerErrorException({
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: err.message,
+        });
+      });
+  }
+
+
 }
--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class NotificationService {
+  constructor(
+    @InjectRepository(Notification)
+    private notificationRepository: Repository<Notification>,
+
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) { }
+
   //user_id에 따라 알림을 불러오는 로직 (최근 10개만 불러오도록)
   //user_id로 알림을 등록하는 로직
   //user_id, notification_id로 알림을 확인을 체크하는 로직
+  async loadNotifications(user_id: string) {
+
+  }
+
+  async isRead(notification_id: string, user_id: string) {
+
+  }
 }
